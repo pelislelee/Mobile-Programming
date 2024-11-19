f26c540f172f55dcbd9e9e12cd638ecc03af1834
amnnic <amnnic07@gmail.com>
2024-11-19 21:53:43 +0700

    progresss

--- lib/book_screen.dart
+++ lib/book_screen.dart
@@ -2,72 +2,82 @@ import 'package:flutter/material.dart';
 import 'genre_tag.dart';
 
 class BookScreen extends StatelessWidget {
-  const BookScreen({super.key});
+  final String title;
+  final String imagePath;
+  final String description;
+  final List<String> genres;
+
+  const BookScreen({
+    super.key,
+    required this.title,
+    required this.imagePath,
+    required this.description,
+    required this.genres,
+    });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[200],
       appBar: AppBar(
         backgroundColor: const Color(0xFFF6B047),
-        elevation: 0,
-        title: const Text('Hi user!',
-        style: TextStyle(color: Colors.white, fontSize: 16),
+        title: Text(title),
         ),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.search, color: Colors.white,),
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: const Icon(Icons.person, color: Colors.white),
-            onPressed: () {},
-          ),
-        ],
-      ),
-      body: Center(
-        child: Container(
-          padding: const EdgeInsets.all(16.0),
-          margin: const EdgeInsets.all(16.0),
-          decoration: BoxDecoration(
-            color: Colors.pink[100],
-            borderRadius: BorderRadius.circular(16),
-          ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Container(
-                width: 100,
-                height: 120,
-                decoration: BoxDecoration(
-                  color: Colors.blue[300],
-                  borderRadius: BorderRadius.circular(8),
+        body: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  height: 200,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(imagePath),
+                    fit: BoxFit.cover,
+                    ),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
                 ),
-              ),
-              const SizedBox(height: 16),
-              const Text(
-                'Yellow Hearts',
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),
-              ),
-              const SizedBox(height: 8),
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  GenreTag(genre: 'Romance'),
-                  GenreTag(genre: 'High-School'),
-                  GenreTag(genre: 'Comedy'),
-                ],
-              ),
-              const SizedBox(height: 16),
-              const Text(
-                'In a bright Monday morning.....',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 14, color: Colors.white, fontFamily: 'Cursive'),
-              ),
-            ],
-          ),
+                const SizedBox(height: 16),
+
+                Text(
+                  title,
+                  style: const TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 8),
+
+                Wrap(
+                  spacing: 8,
+                  runSpacing: 8,
+                  children: genres
+                  .map((genre) => GenreTag(
+                    genre: genre,
+                  backgroundColor: Colors.orange[300]!,
+                  ))
+                  .toList(),
+                ),
+                const SizedBox(height: 16),
+
+                Text(
+                  'Description:',
+                  style: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.orange,
+                  ),
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  description,
+                  style: const TextStyle(fontSize: 16),
+                  textAlign: TextAlign.justify,
+                )
+              ],
+            )),
         ),
-      ),
-    );
+      );
   }
 }
