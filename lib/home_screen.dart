f96acad93aad98dccc014dfcab15ad28de30eec1
535230128 Felice Anastasia Lee <feliceanastasia25@gmail.com>
2024-11-28 15:25:12 +0700

    the giver bookcover

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -274,7 +274,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 },
                 {
                   'title': ' The Giver ', 
-                  'image': 'lib/images/bookcovers/thegiver.avif',
+                  'image': 'lib/images/bookcovers/thegiver.png',
                   'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
                   'genres': ['Sci-fi', 'Action', 'Drama', 'Adventure']
                 },
