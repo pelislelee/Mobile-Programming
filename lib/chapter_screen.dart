03d52e2cd09b74229a2b020c65f568454fa22f68
Maria Melisa H <mmelisaher@gmail.com>
2024-11-29 12:16:50 +0700

    ubah modifikasi chapter screen

--- lib/chapter_screen.dart
+++ lib/chapter_screen.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:lorem_ipsum/lorem_ipsum.dart';
-import 'theme_provider.dart'; // impor ThemeProvider
-import 'text_size_provider.dart'; // impor TextSizeProvider
+import 'theme_provider.dart'; // Import ThemeProvider
+import 'text_size_provider.dart'; // Import TextSizeProvider
 
 class ChapterScreen extends StatelessWidget {
-  const ChapterScreen({super.key});
+  final int chapterIndex; // Index untuk chapter
+
+  const ChapterScreen({super.key, required this.chapterIndex});
 
   @override
   Widget build(BuildContext context) {
+    final chapters = [
+      {'title': 'Chapter 1: Introduction', 'content': loremIpsum(paragraphs: 3, words: 150)},
+      {'title': 'Chapter 2: Advanced Concepts', 'content': loremIpsum(paragraphs: 4, words: 200)},
+      {'title': 'Chapter 3: Final Thoughts', 'content': loremIpsum(paragraphs: 3, words: 150)},
+    ];
+
     return Consumer2<ThemeProvider, TextSizeProvider>(
       builder: (context, themeProvider, textSizeProvider, child) {
         return Scaffold(
@@ -16,9 +24,7 @@ class ChapterScreen extends StatelessWidget {
               ? const Color(0xFF121212) // Dark mode background
               : const Color(0xFFFef3f1), // Light mode background
           appBar: AppBar(
-            backgroundColor: themeProvider.isDarkMode
-                ? Colors.black
-                : Colors.white,
+            backgroundColor: themeProvider.isDarkMode ? Colors.black : Colors.white,
             elevation: 0,
             leading: IconButton(
               icon: const Icon(Icons.arrow_back, color: Colors.orange),
@@ -29,12 +35,8 @@ class ChapterScreen extends StatelessWidget {
             actions: [
               IconButton(
                 icon: Icon(
-                  themeProvider.isDarkMode
-                      ? Icons.dark_mode
-                      : Icons.light_mode,
-                  color: themeProvider.isDarkMode
-                      ? Colors.yellow
-                      : Colors.orange,
+                  themeProvider.isDarkMode ? Icons.dark_mode : Icons.light_mode,
+                  color: themeProvider.isDarkMode ? Colors.yellow : Colors.orange,
                 ),
                 onPressed: () {
                   themeProvider.toogleTheme(); // Toggle dark/light mode
@@ -53,7 +55,7 @@ class ChapterScreen extends StatelessWidget {
               IconButton(
                 icon: const Icon(Icons.format_size, color: Colors.red),
                 onPressed: () {
-                  //logika untuk mengubah ukuran teks
+                  // Logic to adjust text size
                   showDialog(
                     context: context,
                     builder: (context) => AlertDialog(
@@ -66,27 +68,11 @@ class ChapterScreen extends StatelessWidget {
                             children: [
                               IconButton(
                                 icon: const Icon(Icons.zoom_out),
-                                onPressed: () {
-                                  textSizeProvider.decreaseTextSize();
-                                  ScaffoldMessenger.of(context).showSnackBar(
-                                    const SnackBar(
-                                      content: Text('Text size decreased'),
-                                      duration: Duration(seconds: 1),
-                                    ),
-                                  );
-                                },
+                                onPressed: textSizeProvider.decreaseTextSize,
                               ),
                               IconButton(
                                 icon: const Icon(Icons.zoom_in),
-                                onPressed: () {
-                                  textSizeProvider.increaseTextSize();
-                                  ScaffoldMessenger.of(context).showSnackBar(
-                                    const SnackBar(
-                                      content: Text('Text size increased'),
-                                      duration: Duration(seconds: 1),
-                                    ),
-                                  );
-                                },
+                                onPressed: textSizeProvider.increaseTextSize,
                               ),
                             ],
                           ),
@@ -107,50 +93,63 @@ class ChapterScreen extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                const Center(
+                Center(
                   child: Text(
-                    'Chapter 1: Introduction',
-                    style: TextStyle(
+                    chapters[chapterIndex]['title'] as String,
+                    style: const TextStyle(
                       fontSize: 24,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
                 ),
-                const SizedBox(height: 20),
+                const SizedBox(height: 16),
                 Expanded(
-                  child: Container(
-                    padding: const EdgeInsets.all(16),
-                    decoration: BoxDecoration(
-                      color: themeProvider.isDarkMode
-                          ? Colors.grey[850]
-                          : Colors.white,
-                      border: Border.all(
-                          color: themeProvider.isDarkMode
-                              ? Colors.purpleAccent
-                              : Colors.purple,
-                          width: 2),
-                      borderRadius: BorderRadius.circular(8),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.1),
-                          blurRadius: 8,
-                          offset: const Offset(0, 4),
-                        ),
-                      ],
-                    ),
-                    child: SingleChildScrollView(
-                      child: Text(
-                        loremIpsum(paragraphs: 3, words: 150), // Placeholder teks
-                        style: TextStyle(
-                          fontSize: textSizeProvider.textSize,
-                          height: 1.8,
-                          color: themeProvider.isDarkMode
-                              ? Colors.white
-                              : Colors.black87,
-                        ),
+                  child: SingleChildScrollView(
+                    child: Text(
+                      chapters[chapterIndex]['content'] as String,
+                      style: TextStyle(
+                        fontSize: textSizeProvider.textSize,
+                        height: 1.8,
+                        color: themeProvider.isDarkMode
+                            ? Colors.white
+                            : Colors.black87,
                       ),
                     ),
                   ),
+                ),
+                const SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    if (chapterIndex > 0)
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => ChapterScreen(
+                                chapterIndex: chapterIndex - 1,
+                              ),
+                            ),
+                          );
+                        },
+                        child: const Text('Previous Chapter'),
+                      ),
+                    if (chapterIndex < chapters.length - 1)
+                      ElevatedButton(
+                        onPressed: () {
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => ChapterScreen(
+                                chapterIndex: chapterIndex + 1,
+                              ),
+                            ),
+                          );
+                        },
+                        child: const Text('Next Chapter'),
+                      ),
+                  ],
                 ),
               ],
             ),
